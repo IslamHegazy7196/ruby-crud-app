971bcd0449b82cd85f4c00479b9b0b36053dbd83
IslamHegazy7196 <islamfathihegazy04@gmail.com>
2022-09-23 04:39:18 +0200

    create&show article

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root 'welcome#home'
   get 'welcome/about', to:'welcome#about'
+  resources :articles
   # Defines the root path route ("/")
   # root "articles#index"
 end
