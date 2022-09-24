f6d84753cf18e0a2979b5d303e0f9825b9690769
IslamHegazy7196 <islamfathihegazy04@gmail.com>
2022-09-24 02:09:33 +0200

    crud & parials

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,13 @@
 class ArticlesController < ApplicationController
- 
+
+  def index
+    @articles=Article.all  
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def new
     @article = Article.new
   end
@@ -8,16 +16,34 @@ class ArticlesController < ApplicationController
     # render plain: params[:article].inspectnew
     @article = Article.new(article_params)
     if @article.save
-      flash[:notice]="article was successfully created"
+      flash[:notice] = 'article was successfully created'
       redirect_to article_path(@article)
     else
       render :new, status: :unprocessable_entity
     end
   end
 
+  def destroy
+    @article=Article.find(params[:id])
+    @article.destroy
+    flash[:notice] = 'Article was successfully deleted'
+    redirect_to articles_path
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = 'article was successfully edited'
+      redirect_to article_path(@article)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   def show
     @article = Article.find(params[:id])
   end
+
   private
     def article_params
       params.require(:article).permit(:title, :description)
